33ea6551704e688e7f965ae3ea65125b706ecb20
chanwoo hyun <guscksdn123@gmail.com>
2025-05-28 20:36:00 +0900

    Enhance CPU information retrieval by adding brand string, feature detection, cache info, and topology detection

--- kernel/include/cpu/cpu.h
+++ kernel/include/cpu/cpu.h
@@ -5,19 +5,122 @@
 
 /* CPU vendor string length */
 #define CPU_VENDOR_STRING_LEN 13
+#define CPU_BRAND_STRING_LEN 49
+
+/* CPU feature flags */
+typedef struct {
+    bool fpu;           /* Floating Point Unit */
+    bool vme;           /* Virtual Mode Extension */
+    bool de;            /* Debugging Extension */
+    bool pse;           /* Page Size Extension */
+    bool tsc;           /* Time Stamp Counter */
+    bool msr;           /* Model Specific Registers */
+    bool pae;           /* Physical Address Extension */
+    bool mce;           /* Machine Check Exception */
+    bool cx8;           /* CMPXCHG8 instruction */
+    bool apic;          /* Advanced Programmable Interrupt Controller */
+    bool sep;           /* Fast System Call */
+    bool mtrr;          /* Memory Type Range Registers */
+    bool pge;           /* Page Global Enable */
+    bool mca;           /* Machine Check Architecture */
+    bool cmov;          /* Conditional Move */
+    bool pat;           /* Page Attribute Table */
+    bool pse36;         /* 36-bit Page Size Extension */
+    bool psn;           /* Processor Serial Number */
+    bool clfsh;         /* Cache Line Flush */
+    bool ds;            /* Debug Store */
+    bool acpi;          /* ACPI support */
+    bool mmx;           /* MMX Technology */
+    bool fxsr;          /* Fast floating point save/restore */
+    bool sse;           /* Streaming SIMD Extensions */
+    bool sse2;          /* Streaming SIMD Extensions 2 */
+    bool ss;            /* Self Snoop */
+    bool htt;           /* Hyper-Threading Technology */
+    bool tm;            /* Thermal Monitor */
+    bool ia64;          /* IA64 processor emulating x86 */
+    bool pbe;           /* Pending Break Enable */
+    bool sse3;          /* Streaming SIMD Extensions 3 */
+    bool pclmulqdq;     /* PCLMULQDQ instruction */
+    bool dtes64;        /* 64-bit DS Area */
+    bool monitor;       /* MONITOR/MWAIT */
+    bool ds_cpl;        /* CPL Qualified Debug Store */
+    bool vmx;           /* Virtual Machine Extensions */
+    bool smx;           /* Safer Mode Extensions */
+    bool est;           /* Enhanced SpeedStep */
+    bool tm2;           /* Thermal Monitor 2 */
+    bool ssse3;         /* Supplemental Streaming SIMD Extensions 3 */
+    bool cnxt_id;       /* Context ID */
+    bool sdbg;          /* Silicon Debug */
+    bool fma;           /* Fused Multiply Add */
+    bool cx16;          /* CMPXCHG16B */
+    bool xtpr;          /* xTPR Update Control */
+    bool pdcm;          /* Perfmon and Debug Capability */
+    bool pcid;          /* Process-context identifiers */
+    bool dca;           /* Direct Cache Access */
+    bool sse4_1;        /* Streaming SIMD Extensions 4.1 */
+    bool sse4_2;        /* Streaming SIMD Extensions 4.2 */
+    bool x2apic;        /* Extended xAPIC */
+    bool movbe;         /* MOVBE instruction */
+    bool popcnt;        /* POPCNT instruction */
+    bool tsc_deadline;  /* TSC-Deadline */
+    bool aes;           /* AES instruction set */
+    bool xsave;         /* XSAVE/XRSTOR/XSETBV/XGETBV */
+    bool osxsave;       /* XSAVE enabled by OS */
+    bool avx;           /* Advanced Vector Extensions */
+    bool f16c;          /* F16C (half-precision) FP support */
+    bool rdrand;        /* RDRAND instruction */
+    bool hypervisor;    /* Running on a hypervisor */
+} cpu_features_t;
+
+/* CPU cache information */
+typedef struct {
+    uint32_t l1_data_cache_size;        /* L1 data cache size in KB */
+    uint32_t l1_instruction_cache_size; /* L1 instruction cache size in KB */
+    uint32_t l2_cache_size;             /* L2 cache size in KB */
+    uint32_t l3_cache_size;             /* L3 cache size in KB */
+    uint32_t cache_line_size;           /* Cache line size in bytes */
+} cpu_cache_info_t;
 
 /* CPU information structure */
 typedef struct {
-    char vendor[CPU_VENDOR_STRING_LEN];  /* CPU vendor string */
-    uint32_t max_cpuid;                  /* Maximum CPUID level */
-    bool cpuid_available;                /* Whether CPUID is available */
+    char vendor[CPU_VENDOR_STRING_LEN];     /* CPU vendor string */
+    char brand[CPU_BRAND_STRING_LEN];       /* CPU brand string */
+    uint32_t max_cpuid;                     /* Maximum CPUID level */
+    uint32_t max_extended_cpuid;            /* Maximum extended CPUID level */
+    bool cpuid_available;                   /* Whether CPUID is available */
+    
+    /* CPU identification */
+    uint32_t family;                        /* CPU family */
+    uint32_t model;                         /* CPU model */
+    uint32_t stepping;                      /* CPU stepping */
+    uint32_t signature;                     /* CPU signature */
+    
+    /* Core and thread information */
+    uint32_t logical_processors;           /* Number of logical processors */
+    uint32_t physical_cores;               /* Number of physical cores */
+    uint32_t threads_per_core;             /* Number of threads per core */
+    
+    /* CPU features */
+    cpu_features_t features;               /* CPU feature flags */
+    
+    /* Cache information */
+    cpu_cache_info_t cache;                /* CPU cache information */
+    
+    /* Frequency information */
+    uint32_t base_frequency;               /* Base frequency in MHz */
+    uint32_t max_frequency;                /* Max frequency in MHz */
+    uint32_t bus_frequency;                /* Bus frequency in MHz */
 } cpu_info_t;
 
 /* CPU detection functions */
 bool cpu_detect_cpuid(void);
 void cpu_get_vendor(char* vendor);
+void cpu_get_brand(char* brand);
 bool cpu_get_info(cpu_info_t* info);
 void cpu_print_info(const cpu_info_t* info);
+void cpu_detect_features(cpu_features_t* features);
+void cpu_detect_cache_info(cpu_cache_info_t* cache);
+void cpu_detect_topology(cpu_info_t* info);
 
 /* CPUID instruction wrapper */
 void cpuid(uint32_t leaf, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx);
--- kernel/src/cpu/cpu.c
+++ kernel/src/cpu/cpu.c
@@ -60,6 +60,209 @@ void cpu_get_vendor(char* vendor) {
     vendor[12] = '\0'; /* Null-terminate string */
 }
 
+/* Get CPU brand string */
+void cpu_get_brand(char* brand) {
+    uint32_t eax, ebx, ecx, edx;
+    
+    /* Check if extended CPUID is available */
+    cpuid(0x80000000, &eax, &ebx, &ecx, &edx);
+    if (eax < 0x80000004) {
+        strcpy(brand, "Unknown");
+        return;
+    }
+    
+    /* Get brand string from CPUID 0x80000002-0x80000004 */
+    cpuid(0x80000002, &eax, &ebx, &ecx, &edx);
+    ((uint32_t*)brand)[0] = eax;
+    ((uint32_t*)brand)[1] = ebx;
+    ((uint32_t*)brand)[2] = ecx;
+    ((uint32_t*)brand)[3] = edx;
+    
+    cpuid(0x80000003, &eax, &ebx, &ecx, &edx);
+    ((uint32_t*)brand)[4] = eax;
+    ((uint32_t*)brand)[5] = ebx;
+    ((uint32_t*)brand)[6] = ecx;
+    ((uint32_t*)brand)[7] = edx;
+    
+    cpuid(0x80000004, &eax, &ebx, &ecx, &edx);
+    ((uint32_t*)brand)[8] = eax;
+    ((uint32_t*)brand)[9] = ebx;
+    ((uint32_t*)brand)[10] = ecx;
+    ((uint32_t*)brand)[11] = edx;
+    
+    brand[48] = '\0'; /* Null-terminate string */
+    
+    /* Trim leading spaces */
+    char* start = brand;
+    while (*start == ' ') start++;
+    if (start != brand) {
+        strcpy(brand, start);
+    }
+}
+
+/* Detect CPU features */
+void cpu_detect_features(cpu_features_t* features) {
+    uint32_t eax, ebx, ecx, edx;
+    
+    memset(features, 0, sizeof(cpu_features_t));
+    
+    /* Get feature information from CPUID leaf 1 */
+    cpuid(1, &eax, &ebx, &ecx, &edx);
+    
+    /* EDX features (standard features) */
+    features->fpu = (edx & (1 << 0)) != 0;
+    features->vme = (edx & (1 << 1)) != 0;
+    features->de = (edx & (1 << 2)) != 0;
+    features->pse = (edx & (1 << 3)) != 0;
+    features->tsc = (edx & (1 << 4)) != 0;
+    features->msr = (edx & (1 << 5)) != 0;
+    features->pae = (edx & (1 << 6)) != 0;
+    features->mce = (edx & (1 << 7)) != 0;
+    features->cx8 = (edx & (1 << 8)) != 0;
+    features->apic = (edx & (1 << 9)) != 0;
+    features->sep = (edx & (1 << 11)) != 0;
+    features->mtrr = (edx & (1 << 12)) != 0;
+    features->pge = (edx & (1 << 13)) != 0;
+    features->mca = (edx & (1 << 14)) != 0;
+    features->cmov = (edx & (1 << 15)) != 0;
+    features->pat = (edx & (1 << 16)) != 0;
+    features->pse36 = (edx & (1 << 17)) != 0;
+    features->psn = (edx & (1 << 18)) != 0;
+    features->clfsh = (edx & (1 << 19)) != 0;
+    features->ds = (edx & (1 << 21)) != 0;
+    features->acpi = (edx & (1 << 22)) != 0;
+    features->mmx = (edx & (1 << 23)) != 0;
+    features->fxsr = (edx & (1 << 24)) != 0;
+    features->sse = (edx & (1 << 25)) != 0;
+    features->sse2 = (edx & (1 << 26)) != 0;
+    features->ss = (edx & (1 << 27)) != 0;
+    features->htt = (edx & (1 << 28)) != 0;
+    features->tm = (edx & (1 << 29)) != 0;
+    features->ia64 = (edx & (1 << 30)) != 0;
+    features->pbe = (edx & (1 << 31)) != 0;
+    
+    /* ECX features (extended features) */
+    features->sse3 = (ecx & (1 << 0)) != 0;
+    features->pclmulqdq = (ecx & (1 << 1)) != 0;
+    features->dtes64 = (ecx & (1 << 2)) != 0;
+    features->monitor = (ecx & (1 << 3)) != 0;
+    features->ds_cpl = (ecx & (1 << 4)) != 0;
+    features->vmx = (ecx & (1 << 5)) != 0;
+    features->smx = (ecx & (1 << 6)) != 0;
+    features->est = (ecx & (1 << 7)) != 0;
+    features->tm2 = (ecx & (1 << 8)) != 0;
+    features->ssse3 = (ecx & (1 << 9)) != 0;
+    features->cnxt_id = (ecx & (1 << 10)) != 0;
+    features->sdbg = (ecx & (1 << 11)) != 0;
+    features->fma = (ecx & (1 << 12)) != 0;
+    features->cx16 = (ecx & (1 << 13)) != 0;
+    features->xtpr = (ecx & (1 << 14)) != 0;
+    features->pdcm = (ecx & (1 << 15)) != 0;
+    features->pcid = (ecx & (1 << 17)) != 0;
+    features->dca = (ecx & (1 << 18)) != 0;
+    features->sse4_1 = (ecx & (1 << 19)) != 0;
+    features->sse4_2 = (ecx & (1 << 20)) != 0;
+    features->x2apic = (ecx & (1 << 21)) != 0;
+    features->movbe = (ecx & (1 << 22)) != 0;
+    features->popcnt = (ecx & (1 << 23)) != 0;
+    features->tsc_deadline = (ecx & (1 << 24)) != 0;
+    features->aes = (ecx & (1 << 25)) != 0;
+    features->xsave = (ecx & (1 << 26)) != 0;
+    features->osxsave = (ecx & (1 << 27)) != 0;
+    features->avx = (ecx & (1 << 28)) != 0;
+    features->f16c = (ecx & (1 << 29)) != 0;
+    features->rdrand = (ecx & (1 << 30)) != 0;
+    features->hypervisor = (ecx & (1 << 31)) != 0;
+}
+
+/* Detect cache information */
+void cpu_detect_cache_info(cpu_cache_info_t* cache) {
+    uint32_t eax, ebx, ecx, edx;
+    
+    memset(cache, 0, sizeof(cpu_cache_info_t));
+    
+    /* Try to get cache info from CPUID leaf 4 (Intel) */
+    cpuid(4, &eax, &ebx, &ecx, &edx);
+    if (eax != 0) {
+        /* Intel deterministic cache parameters */
+        uint32_t cache_type = eax & 0x1F;
+        uint32_t cache_level = (eax >> 5) & 0x7;
+        uint32_t ways = ((ebx >> 22) & 0x3FF) + 1;
+        uint32_t partitions = ((ebx >> 12) & 0x3FF) + 1;
+        uint32_t line_size = (ebx & 0xFFF) + 1;
+        uint32_t sets = ecx + 1;
+        uint32_t cache_size = ways * partitions * line_size * sets / 1024;
+        
+        cache->cache_line_size = line_size;
+        
+        if (cache_level == 1 && cache_type == 1) {
+            cache->l1_data_cache_size = cache_size;
+        } else if (cache_level == 1 && cache_type == 2) {
+            cache->l1_instruction_cache_size = cache_size;
+        } else if (cache_level == 2) {
+            cache->l2_cache_size = cache_size;
+        } else if (cache_level == 3) {
+            cache->l3_cache_size = cache_size;
+        }
+    }
+    
+    /* Try AMD cache info from extended CPUID */
+    cpuid(0x80000000, &eax, &ebx, &ecx, &edx);
+    if (eax >= 0x80000005) {
+        cpuid(0x80000005, &eax, &ebx, &ecx, &edx);
+        cache->l1_data_cache_size = (ecx >> 24) & 0xFF;
+        cache->l1_instruction_cache_size = (edx >> 24) & 0xFF;
+        if (cache->cache_line_size == 0) {
+            cache->cache_line_size = ecx & 0xFF;
+        }
+    }
+    
+    if (eax >= 0x80000006) {
+        cpuid(0x80000006, &eax, &ebx, &ecx, &edx);
+        cache->l2_cache_size = (ecx >> 16) & 0xFFFF;
+        cache->l3_cache_size = ((edx >> 18) & 0x3FFF) * 512; /* In 512KB units */
+    }
+}
+
+/* Detect CPU topology (cores and threads) */
+void cpu_detect_topology(cpu_info_t* info) {
+    uint32_t eax, ebx, ecx, edx;
+    
+    /* Initialize with defaults */
+    info->logical_processors = 1;
+    info->physical_cores = 1;
+    info->threads_per_core = 1;
+    
+    /* Get basic processor info */
+    cpuid(1, &eax, &ebx, &ecx, &edx);
+    
+    /* Check if HTT is supported */
+    if (edx & (1 << 28)) {
+        info->logical_processors = (ebx >> 16) & 0xFF;
+    }
+    
+    /* Try to get extended topology information */
+    cpuid(0x80000000, &eax, &ebx, &ecx, &edx);
+    if (eax >= 0x80000008) {
+        cpuid(0x80000008, &eax, &ebx, &ecx, &edx);
+        info->physical_cores = (ecx & 0xFF) + 1;
+        if (info->physical_cores > 0) {
+            info->threads_per_core = info->logical_processors / info->physical_cores;
+        }
+    }
+    
+    /* If extended topology didn't work, try Intel's method */
+    if (info->physical_cores == 1) {
+        cpuid(4, &eax, &ebx, &ecx, &edx);
+        if (eax != 0) {
+            info->physical_cores = ((eax >> 26) & 0x3F) + 1;
+            if (info->physical_cores > 0) {
+                info->threads_per_core = info->logical_processors / info->physical_cores;
+            }
+        }
+    }
+}
+
 /* Get comprehensive CPU information */
 bool cpu_get_info(cpu_info_t* info) {
     if (!info) {
@@ -87,6 +290,54 @@ bool cpu_get_info(cpu_info_t* info) {
     ((uint32_t*)info->vendor)[2] = ecx;
     info->vendor[12] = '\0';
     
+    /* Get maximum extended CPUID level */
+    cpuid(0x80000000, &eax, &ebx, &ecx, &edx);
+    info->max_extended_cpuid = eax;
+    
+    /* Get CPU brand string */
+    cpu_get_brand(info->brand);
+    
+    /* Get CPU signature and family/model/stepping */
+    if (info->max_cpuid >= 1) {
+        cpuid(1, &eax, &ebx, &ecx, &edx);
+        info->signature = eax;
+        info->stepping = eax & 0xF;
+        info->model = (eax >> 4) & 0xF;
+        info->family = (eax >> 8) & 0xF;
+        
+        /* Extended family and model for newer CPUs */
+        if (info->family == 0xF) {
+            info->family += (eax >> 20) & 0xFF;
+        }
+        if (info->family == 0xF || info->family == 0x6) {
+            info->model += ((eax >> 16) & 0xF) << 4;
+        }
+    }
+    
+    /* Detect CPU features */
+    cpu_detect_features(&info->features);
+    
+    /* Detect cache information */
+    cpu_detect_cache_info(&info->cache);
+    
+    /* Detect CPU topology */
+    cpu_detect_topology(info);
+    
+    /* Try to get frequency information (if available) */
+    if (info->max_cpuid >= 0x15) {
+        cpuid(0x15, &eax, &ebx, &ecx, &edx);
+        if (eax != 0 && ebx != 0 && ecx != 0) {
+            info->base_frequency = (ecx * ebx) / eax / 1000000; /* Convert to MHz */
+        }
+    }
+    
+    if (info->max_cpuid >= 0x16) {
+        cpuid(0x16, &eax, &ebx, &ecx, &edx);
+        info->base_frequency = eax;
+        info->max_frequency = ebx;
+        info->bus_frequency = ecx;
+    }
+    
     return true;
 }
 
@@ -102,20 +353,154 @@ void cpu_print_info(const cpu_info_t* info) {
         return;
     }
     
-    char num_str[12];
+    char num_str[16];
     
-    terminal_writestring("CPU Information:\n");
+    terminal_writeline("========== CPU Information ==========");
     
-    /* Print vendor */
-    terminal_writestring("  Vendor: ");
+    /* Print vendor and brand */
+    terminal_writestring("Vendor: ");
     terminal_writeline(info->vendor);
     
-    /* Print maximum CPUID level */
-    terminal_writestring("  Max CPUID Level: ");
+    terminal_writestring("Brand: ");
+    terminal_writeline(info->brand);
+    
+    /* Print CPU identification */
+    terminal_writestring("Family: ");
+    int_to_string(info->family, num_str);
+    terminal_writeline(num_str);
+    
+    terminal_writestring("Model: ");
+    int_to_string(info->model, num_str);
+    terminal_writeline(num_str);
+    
+    terminal_writestring("Stepping: ");
+    int_to_string(info->stepping, num_str);
+    terminal_writeline(num_str);
+    
+    /* Print core and thread information */
+    terminal_writeline("\n--- Core/Thread Information ---");
+    
+    terminal_writestring("Physical Cores: ");
+    int_to_string(info->physical_cores, num_str);
+    terminal_writeline(num_str);
+    
+    terminal_writestring("Logical Processors: ");
+    int_to_string(info->logical_processors, num_str);
+    terminal_writeline(num_str);
+    
+    terminal_writestring("Threads per Core: ");
+    int_to_string(info->threads_per_core, num_str);
+    terminal_writeline(num_str);
+    
+    /* Print cache information */
+    terminal_writeline("\n--- Cache Information ---");
+    
+    if (info->cache.l1_data_cache_size > 0) {
+        terminal_writestring("L1 Data Cache: ");
+        int_to_string(info->cache.l1_data_cache_size, num_str);
+        terminal_writestring(num_str);
+        terminal_writeline(" KB");
+    }
+    
+    if (info->cache.l1_instruction_cache_size > 0) {
+        terminal_writestring("L1 Instruction Cache: ");
+        int_to_string(info->cache.l1_instruction_cache_size, num_str);
+        terminal_writestring(num_str);
+        terminal_writeline(" KB");
+    }
+    
+    if (info->cache.l2_cache_size > 0) {
+        terminal_writestring("L2 Cache: ");
+        int_to_string(info->cache.l2_cache_size, num_str);
+        terminal_writestring(num_str);
+        terminal_writeline(" KB");
+    }
+    
+    if (info->cache.l3_cache_size > 0) {
+        terminal_writestring("L3 Cache: ");
+        int_to_string(info->cache.l3_cache_size, num_str);
+        terminal_writestring(num_str);
+        terminal_writeline(" KB");
+    }
+    
+    if (info->cache.cache_line_size > 0) {
+        terminal_writestring("Cache Line Size: ");
+        int_to_string(info->cache.cache_line_size, num_str);
+        terminal_writestring(num_str);
+        terminal_writeline(" bytes");
+    }
+    
+    /* Print frequency information */
+    if (info->base_frequency > 0 || info->max_frequency > 0) {
+        terminal_writeline("\n--- Frequency Information ---");
+        
+        if (info->base_frequency > 0) {
+            terminal_writestring("Base Frequency: ");
+            int_to_string(info->base_frequency, num_str);
+            terminal_writestring(num_str);
+            terminal_writeline(" MHz");
+        }
+        
+        if (info->max_frequency > 0) {
+            terminal_writestring("Max Frequency: ");
+            int_to_string(info->max_frequency, num_str);
+            terminal_writestring(num_str);
+            terminal_writeline(" MHz");
+        }
+        
+        if (info->bus_frequency > 0) {
+            terminal_writestring("Bus Frequency: ");
+            int_to_string(info->bus_frequency, num_str);
+            terminal_writestring(num_str);
+            terminal_writeline(" MHz");
+        }
+    }
+    
+    /* Print CPU features */
+    terminal_writeline("\n--- CPU Features ---");
+    
+    if (info->features.fpu) terminal_writeline("FPU: Yes");
+    if (info->features.tsc) terminal_writeline("TSC: Yes");
+    if (info->features.msr) terminal_writeline("MSR: Yes");
+    if (info->features.pae) terminal_writeline("PAE: Yes");
+    if (info->features.apic) terminal_writeline("APIC: Yes");
+    if (info->features.mmx) terminal_writeline("MMX: Yes");
+    if (info->features.sse) terminal_writeline("SSE: Yes");
+    if (info->features.sse2) terminal_writeline("SSE2: Yes");
+    if (info->features.sse3) terminal_writeline("SSE3: Yes");
+    if (info->features.ssse3) terminal_writeline("SSSE3: Yes");
+    if (info->features.sse4_1) terminal_writeline("SSE4.1: Yes");
+    if (info->features.sse4_2) terminal_writeline("SSE4.2: Yes");
+    if (info->features.avx) terminal_writeline("AVX: Yes");
+    if (info->features.aes) terminal_writeline("AES: Yes");
+    if (info->features.htt) terminal_writeline("Hyper-Threading: Yes");
+    if (info->features.vmx) terminal_writeline("VT-x: Yes");
+    if (info->features.rdrand) terminal_writeline("RDRAND: Yes");
+    if (info->features.hypervisor) terminal_writeline("Hypervisor: Yes");
+    
+    /* Print CPUID levels */
+    terminal_writeline("\n--- CPUID Information ---");
+    
+    terminal_writestring("Max CPUID Level: ");
     int_to_string(info->max_cpuid, num_str);
     terminal_writeline(num_str);
     
-    /* Print CPUID availability */
-    terminal_writestring("  CPUID Available: ");
-    terminal_writeline(info->cpuid_available ? "Yes" : "No");
+    terminal_writestring("Max Extended CPUID Level: 0x");
+    /* Simple hex conversion for extended CPUID */
+    if (info->max_extended_cpuid >= 0x80000000) {
+        int_to_string(info->max_extended_cpuid - 0x80000000, num_str);
+        terminal_writestring("8000");
+        if (info->max_extended_cpuid - 0x80000000 < 16) {
+            terminal_writestring("000");
+        } else if (info->max_extended_cpuid - 0x80000000 < 256) {
+            terminal_writestring("00");
+        } else if (info->max_extended_cpuid - 0x80000000 < 4096) {
+            terminal_writestring("0");
+        }
+        terminal_writeline(num_str);
+    } else {
+        terminal_writeline("Not Available");
+    }
+    
+    terminal_writeline("=====================================");
 }

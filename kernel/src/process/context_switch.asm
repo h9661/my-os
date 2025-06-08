d66b213f308ee25c1d6bb22f040e7d0095296374
chanwoo hyun <guscksdn123@gmail.com>
2025-06-08 20:37:40 +0900

    refactor: 컨텍스트 스위칭 함수 리팩토링
    기존의 save_context와 restore_context 함수를 통합하여
    context_switch 함수로 리팩토링하였습니다. 이를 통해
    코드의 가독성을 높이고 중복을 제거하였습니다.
    
    또한, 레지스터 저장 및 복원 로직을 간소화하여
    성능을 개선하였습니다.

--- kernel/src/process/context_switch.asm
+++ kernel/src/process/context_switch.asm
@@ -1,165 +1,93 @@
 [BITS 32]
 
-; Context switching assembly functions
-global save_context
-global restore_context
+; Context switching function
+; void context_switch(process_regs_t* old_regs, process_regs_t* new_regs)
 global context_switch
 
-; Save current CPU context to memory
-; void save_context(cpu_context_t* context)
-save_context:
-    push ebp
-    mov ebp, esp
+context_switch:
+    ; Get parameters
+    mov eax, [esp + 4]      ; old_regs pointer
+    mov ebx, [esp + 8]      ; new_regs pointer
     
-    ; Get context pointer from argument
-    mov edx, [ebp + 8]
+    ; Save current process state to old_regs
+    test eax, eax
+    jz load_new_context     ; Skip saving if old_regs is NULL
     
     ; Save general purpose registers
-    mov [edx + 0], eax     ; context->eax
-    mov [edx + 4], ebx     ; context->ebx
-    mov [edx + 8], ecx     ; context->ecx
-    mov [edx + 12], edx    ; context->edx (will be overwritten, save original)
-    mov eax, edx           ; save context pointer
-    mov edx, [ebp + 8]     ; restore context pointer
-    mov [eax + 12], edx    ; save original edx
+    mov [eax + 0], eax      ; Save EAX (will be overwritten, but that's ok)
+    mov [eax + 4], ebx      ; Save EBX
+    mov [eax + 8], ecx      ; Save ECX  
+    mov [eax + 12], edx     ; Save EDX
+    mov [eax + 16], esi     ; Save ESI
+    mov [eax + 20], edi     ; Save EDI
+    mov [eax + 24], esp     ; Save ESP
+    mov [eax + 28], ebp     ; Save EBP
     
-    mov [edx + 16], esi    ; context->esi
-    mov [edx + 20], edi    ; context->edi
+    ; Save EIP (return address)
+    mov ecx, [esp]          ; Get return address from stack
+    mov [eax + 32], ecx     ; Save EIP
     
-    ; Save stack pointers
-    mov [edx + 24], esp    ; context->esp
-    mov [edx + 28], ebp    ; context->ebp
-    
-    ; Save instruction pointer (return address)
-    mov eax, [ebp + 4]     ; get return address from stack
-    mov [edx + 32], eax    ; context->eip
-    
-    ; Save flags
-    pushf
-    pop eax
-    mov [edx + 36], eax    ; context->eflags
+    ; Save EFLAGS
+    pushfd                  ; Push EFLAGS onto stack
+    pop ecx                 ; Pop into ECX
+    mov [eax + 36], ecx     ; Save EFLAGS
     
     ; Save segment registers
-    mov ax, cs
-    mov [edx + 40], ax     ; context->cs
-    mov ax, ds
-    mov [edx + 44], ax     ; context->ds
-    mov ax, es
-    mov [edx + 48], ax     ; context->es
-    mov ax, fs
-    mov [edx + 52], ax     ; context->fs
-    mov ax, gs
-    mov [edx + 56], ax     ; context->gs
-    mov ax, ss
-    mov [edx + 60], ax     ; context->ss
-    
-    mov esp, ebp
-    pop ebp
-    ret
+    mov cx, cs
+    mov [eax + 40], cx      ; Save CS
+    mov cx, ds  
+    mov [eax + 42], cx      ; Save DS
+    mov cx, es
+    mov [eax + 44], cx      ; Save ES
+    mov cx, fs
+    mov [eax + 46], cx      ; Save FS
+    mov cx, gs
+    mov [eax + 48], cx      ; Save GS
+    mov cx, ss
+    mov [eax + 50], cx      ; Save SS
 
-; Restore CPU context from memory
-; void restore_context(cpu_context_t* context)
-restore_context:
-    ; Get context pointer from stack (we can't use standard calling convention
-    ; since we're changing all registers)
-    mov edx, [esp + 4]
+load_new_context:
+    ; Load new process state from new_regs
+    test ebx, ebx
+    jz context_switch_done  ; Skip loading if new_regs is NULL
     
-    ; Restore segment registers
-    mov ax, [edx + 44]     ; context->ds
-    mov ds, ax
-    mov ax, [edx + 48]     ; context->es
-    mov es, ax
-    mov ax, [edx + 52]     ; context->fs
-    mov fs, ax
-    mov ax, [edx + 56]     ; context->gs
-    mov gs, ax
-    mov ax, [edx + 60]     ; context->ss
-    mov ss, ax
+    ; Load segment registers first
+    mov cx, [ebx + 42]      ; Load DS
+    mov ds, cx
+    mov cx, [ebx + 44]      ; Load ES
+    mov es, cx
+    mov cx, [ebx + 46]      ; Load FS
+    mov fs, cx
+    mov cx, [ebx + 48]      ; Load GS
+    mov gs, cx
+    mov cx, [ebx + 50]      ; Load SS
+    mov ss, cx
     
-    ; Restore stack pointers
-    mov esp, [edx + 24]    ; context->esp
-    mov ebp, [edx + 28]    ; context->ebp
+    ; Load stack pointer
+    mov esp, [ebx + 24]     ; Load ESP
+    mov ebp, [ebx + 28]     ; Load EBP
     
-    ; Restore flags
-    mov eax, [edx + 36]    ; context->eflags
-    push eax
-    popf
+    ; Load EFLAGS
+    mov ecx, [ebx + 36]     ; Load EFLAGS
+    push ecx                ; Push onto stack
+    popfd                   ; Pop into EFLAGS register
     
-    ; Push instruction pointer for iret
-    push dword [edx + 40]  ; context->cs
-    push dword [edx + 32]  ; context->eip
+    ; Load general purpose registers
+    mov eax, [ebx + 0]      ; Load EAX
+    mov ecx, [ebx + 8]      ; Load ECX
+    mov edx, [ebx + 12]     ; Load EDX
+    mov esi, [ebx + 16]     ; Load ESI
+    mov edi, [ebx + 20]     ; Load EDI
     
-    ; Restore general purpose registers
-    mov eax, [edx + 0]     ; context->eax
-    mov ebx, [edx + 4]     ; context->ebx
-    mov ecx, [edx + 8]     ; context->ecx
-    mov esi, [edx + 16]    ; context->esi
-    mov edi, [edx + 20]    ; context->edi
-    ; Note: EDX is restored last since we're using it
+    ; Load EIP and CS by setting up return
+    push dword [ebx + 40]   ; Push CS
+    push dword [ebx + 32]   ; Push EIP
     
-    ; Save and restore EDX
-    push dword [edx + 12]  ; context->edx
-    pop edx
+    ; Load EBX last
+    mov ebx, [ebx + 4]      ; Load EBX
     
-    ; Far return to restore CS:EIP
+    ; Far return to new process
     retf
 
-; Perform context switch between two processes
-; void context_switch(process_t* old_process, process_t* new_process)
-context_switch:
-    push ebp
-    mov ebp, esp
-    push eax
-    push ebx
-    push ecx
-    push edx
-    push esi
-    push edi
-    
-    ; Get process pointers
-    mov eax, [ebp + 8]     ; old_process
-    mov ebx, [ebp + 12]    ; new_process
-    
-    ; Save current context to old process
-    ; Offset to context field in process structure (assuming it's at offset 16)
-    add eax, 16            ; &old_process->context
-    push eax
-    call save_context
-    add esp, 4
-    
-    ; Restore context from new process
-    ; Offset to context field in process structure
-    add ebx, 16            ; &new_process->context
-    push ebx
-    call restore_context
-    add esp, 4
-    
-    ; This point should not be reached in normal execution
-    ; as restore_context should jump to the new process
-    
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov esp, ebp
-    pop ebp
+context_switch_done:
     ret
-
-; Simple process entry wrapper
-; This is used to wrap process entry points
-global process_entry_wrapper
-process_entry_wrapper:
-    ; Enable interrupts for the process
-    sti
-    
-    ; Call the actual process function
-    ; The function address should be set up by the scheduler
-    call eax
-    
-    ; If process returns, terminate it
-    ; In a real OS, this would call process_exit() or similar
-.loop:
-    hlt
-    jmp .loop

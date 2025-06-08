7bb3d0263ef489b215b348dbf0e755a46309d969
chanwoo hyun <guscksdn123@gmail.com>
2025-06-08 20:37:32 +0900

    feat: 프로세스 상태 및 우선순위 구조 개선
    프로세스 상태를 추가하고 우선순위 레벨을 재정의하여
    시스템 프로세스에 대한 우선순위를 명확히 하였습니다.
    
    프로세스 제어 블록(PCB) 구조를 수정하여
    부모 프로세스 ID와 메모리 관리 필드를 추가하였습니다.
    
    스케줄러 구조를 개선하여
    현재 프로세스와 대기 큐를 보다 효율적으로 관리할 수 있도록 하였습니다.

--- kernel/include/process/process.h
+++ kernel/include/process/process.h
@@ -5,124 +5,136 @@
 
 /* Process states */
 typedef enum {
-    PROCESS_STATE_READY = 0,
-    PROCESS_STATE_RUNNING,
+    PROCESS_STATE_RUNNING = 0,
+    PROCESS_STATE_READY,
     PROCESS_STATE_BLOCKED,
-    PROCESS_STATE_TERMINATED
+    PROCESS_STATE_SLEEPING,
+    PROCESS_STATE_TERMINATED,
+    PROCESS_STATE_ZOMBIE
 } process_state_t;
 
 /* Process priority levels */
 typedef enum {
-    PROCESS_PRIORITY_LOW = 0,
-    PROCESS_PRIORITY_NORMAL,
-    PROCESS_PRIORITY_HIGH
+    PROCESS_PRIORITY_SYSTEM = 0,    /* Highest priority for system processes */
+    PROCESS_PRIORITY_HIGH = 1,      /* High priority */
+    PROCESS_PRIORITY_NORMAL = 2,    /* Normal priority */
+    PROCESS_PRIORITY_LOW = 3        /* Low priority */
 } process_priority_t;
 
-/* CPU context structure for context switching */
-typedef struct {
-    uint32_t eax, ebx, ecx, edx;    /* General purpose registers */
-    uint32_t esi, edi;              /* Index registers */
-    uint32_t esp, ebp;              /* Stack pointers */
-    uint32_t eip;                   /* Instruction pointer */
-    uint32_t eflags;                /* Flags register */
-    uint32_t cs, ds, es, fs, gs, ss; /* Segment registers */
-} cpu_context_t;
+/* Maximum number of processes */
+#define MAX_PROCESSES 32
 
-/* Process Control Block (PCB) */
+/* Process ID type */
+typedef uint32_t pid_t;
+
+/* Process register state for context switching */
+typedef struct {
+    uint32_t eax, ebx, ecx, edx;
+    uint32_t esi, edi, esp, ebp;
+    uint32_t eip, eflags;
+    uint16_t cs, ds, es, fs, gs, ss;
+} __attribute__((packed)) process_regs_t;
+
+/* Process control block (PCB) */
 typedef struct process {
-    uint32_t pid;                   /* Process ID */
-    char name[32];                  /* Process name */
-    process_state_t state;          /* Current state */
+    pid_t pid;                      /* Process ID */
+    pid_t parent_pid;               /* Parent process ID */
+    process_state_t state;          /* Current process state */
     process_priority_t priority;    /* Process priority */
     
-    cpu_context_t context;          /* CPU context for context switching */
-    
+    /* Memory management */
     uint32_t stack_base;            /* Stack base address */
     uint32_t stack_size;            /* Stack size */
-    uint32_t stack_pointer;         /* Current stack pointer */
+    uint32_t heap_base;             /* Heap base address */
+    uint32_t heap_size;             /* Heap size */
     
-    uint32_t time_slice;            /* Time slice for scheduling */
-    uint32_t time_used;             /* Time used in current slice */
+    /* CPU state */
+    process_regs_t regs;            /* CPU registers */
+    uint32_t kernel_stack;          /* Kernel stack pointer */
     
-    struct process *next;           /* Next process in queue */
-    struct process *prev;           /* Previous process in queue */
+    /* Timing */
+    uint32_t creation_time;         /* Process creation time */
+    uint32_t cpu_time;              /* Total CPU time used */
+    uint32_t sleep_until;           /* Wake up time (for sleeping processes) */
+    
+    /* Process management */
+    int exit_code;                  /* Exit code when terminated */
+    struct process* parent;         /* Pointer to parent process */
+    struct process* next;           /* Next process in list */
+    struct process* prev;           /* Previous process in list */
+    
+    /* Process name */
+    char name[32];                  /* Process name */
 } process_t;
 
-/* Process queue structure */
+/* Scheduler state */
 typedef struct {
-    process_t *head;
-    process_t *tail;
-    uint32_t count;
-} process_queue_t;
-
-/* Scheduler structure */
-typedef struct {
-    process_t *current_process;     /* Currently running process */
-    process_queue_t ready_queue;    /* Ready processes queue */
-    process_queue_t blocked_queue;  /* Blocked processes queue */
-    uint32_t next_pid;             /* Next available PID */
-    uint32_t process_count;        /* Total number of processes */
+    process_t* current_process;     /* Currently running process */
+    process_t* ready_queue_head;    /* Head of ready queue */
+    process_t* ready_queue_tail;    /* Tail of ready queue */
+    process_t* sleeping_queue;      /* Sleeping processes queue */
+    uint32_t num_processes;         /* Total number of processes */
+    uint32_t scheduler_ticks;       /* Scheduler tick counter */
+    bool preemption_enabled;        /* Preemptive scheduling enabled */
 } scheduler_t;
+
+/* Global scheduler instance - defined in process.c */
+extern scheduler_t scheduler;
 
 /* Process management functions */
 void process_init(void);
 process_t* process_create(const char* name, void (*entry_point)(void), process_priority_t priority);
-void process_destroy(process_t* process);
+process_t* process_fork(process_t* parent);
+void process_terminate(process_t* process);
+int process_kill(process_t* process, int signal);
+int process_wait(process_t* parent, pid_t child_pid);
+int process_exec(process_t* process, const char* path, char* const argv[]);
+
+/* Process state management */
+void process_sleep(process_t* process, uint32_t milliseconds);
+void process_wake_up(process_t* process);
 void process_yield(void);
 void process_block(process_t* process);
 void process_unblock(process_t* process);
-void process_set_priority(process_t* process, process_priority_t priority);
 
-/* Process control functions */
+/* Process lookup */
 process_t* get_current_process(void);
-void process_terminate(process_t* process);
-process_t* process_fork(process_t* parent);
-void process_sleep(process_t* process, uint32_t milliseconds);
-int process_kill(process_t* process, int signal);
-int process_wait(process_t* parent, uint32_t child_pid);
-int process_exec(process_t* process, const char* path, char* const argv[]);
-
-/* Signal handling */
-#define SIGTERM 15
-#define SIGKILL 9
-#define SIGSTOP 19
-#define SIGCONT 18
-
-typedef struct {
-    int signal;
-    uint32_t timestamp;
-} signal_t;
-
-/* Process synchronization */
-void process_mutex_init(void);
-int process_mutex_lock(uint32_t mutex_id);
-int process_mutex_unlock(uint32_t mutex_id);
+process_t* process_find_by_pid(pid_t pid);
+pid_t process_get_next_pid(void);
 
 /* Scheduler functions */
 void scheduler_init(void);
 void scheduler_add_process(process_t* process);
 void scheduler_remove_process(process_t* process);
-process_t* scheduler_get_next_process(void);
 void scheduler_switch_process(void);
 void scheduler_tick(void);
+void scheduler_set_preemption(bool enabled);
 
-/* Context switching functions */
-void context_switch(process_t* old_process, process_t* new_process);
-void save_context(cpu_context_t* context);
-void restore_context(cpu_context_t* context);
+/* Context switching (implemented in assembly) */
+extern void context_switch(process_regs_t* old_regs, process_regs_t* new_regs);
 
-/* Process information functions */
-void process_list_all(void);
-process_t* process_find_by_pid(uint32_t pid);
+/* Process debugging and information */
+void process_print_info(process_t* process);
+void process_print_all(void);
 uint32_t process_get_count(void);
-void process_debug_info(process_t* process);
 
-/* Process states management */
-void process_set_state(process_t* process, process_state_t state);
-const char* process_state_to_string(process_state_t state);
+/* Signal handling */
+#define SIGNAL_TERM     1   /* Terminate */
+#define SIGNAL_KILL     2   /* Kill (cannot be caught) */
+#define SIGNAL_STOP     3   /* Stop */
+#define SIGNAL_CONT     4   /* Continue */
 
-/* Stack management for processes */
-uint32_t allocate_process_stack(uint32_t size);
-void free_process_stack(uint32_t stack_base, uint32_t size);
+/* Memory allocation for processes */
+void* process_allocate_memory(process_t* process, size_t size);
+void process_free_memory(process_t* process, void* ptr);
+
+/* Process timing functions */
+uint32_t process_get_cpu_time(process_t* process);
+uint32_t process_get_uptime(process_t* process);
+
+/* Context switching test functions */
+void process_test_context_switching(void);
+void test_process_a(void);
+void test_process_b(void);
 
 #endif /* PROCESS_H */
